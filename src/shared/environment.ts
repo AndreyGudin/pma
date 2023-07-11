9cb460353ba033d14131115cd873582a4f09e91d
Andrey Gudin <aeon150791@gmail.com>
2023-07-11 21:42:07 +0400

    feat: update connection string

--- src/shared/environment.ts
+++ src/shared/environment.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://hidden-spire-47117.herokuapp.com';
+export const BASE_URL = 'https://final-task-backend-production-b717.up.railway.app';
 // export const sessionLifetime = 86400000; // 24h
 export const sessionLifetime = 1800000; // 30min
 // export const sessionCheckInterval = 300000; // 5min
